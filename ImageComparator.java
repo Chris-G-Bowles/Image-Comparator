908137890d6569c98b33281585de6f5512423836
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-23 23:04:18 -0700

    Reworked first error case.

--- ImageComparator.java
+++ ImageComparator.java
@@ -11,48 +11,47 @@ import javax.swing.JFrame;
 public class ImageComparator {
 	public static void main(String[] args) {
 		System.out.println("* Image Comparator *");
-		if (args.length == 0 || args.length == 1) {
-			Scanner input = new Scanner(System.in);
-			String directoryLocation;
-			if (args.length == 0) {
-				System.out.print("Enter a directory location containing 2 or more images: ");
-				directoryLocation = input.nextLine();
-			} else {
-				directoryLocation = args[0];
-			}
-			input.close();
-			File directory = new File(directoryLocation);
-			if (directory.isDirectory()) {
-				System.out.println("(Please wait a few seconds for the images to load.)");
-				ArrayList<ImageIcon> images = addImagesFromDirectory(directory);
-				if (images.size() >= 2) {
-					System.out.println("-----");
-					JFrame frame = new JFrame("Image Comparator");
-					ImageComparatorPanel panel = new ImageComparatorPanel(frame, images);
-					frame.setContentPane(panel);
-					int frameWidth = 1280;
-					int frameHeight = 720;
-					if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
-						frameHeight += 22;
-					} else if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
-						frameWidth += 16;
-						frameHeight += 39;
-					}
-					frame.setSize(frameWidth, frameHeight);
-					frame.setLocationRelativeTo(null);
-					frame.setResizable(false);
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setVisible(true);
-				} else {
-					System.out.println("Error: " + directoryLocation + " contains less than 2 images.");
+		if (args.length != 0 && args.length != 1) {
+			error("This program's usage is as follows:\n" +
+					"java ImageComparator\n" +
+					"java ImageComparator <directory location>");
+		}
+		Scanner input = new Scanner(System.in);
+		String directoryLocation;
+		if (args.length == 0) {
+			System.out.print("Enter a directory location containing 2 or more images: ");
+			directoryLocation = input.nextLine();
+		} else {
+			directoryLocation = args[0];
+		}
+		input.close();
+		File directory = new File(directoryLocation);
+		if (directory.isDirectory()) {
+			System.out.println("(Please wait a few seconds for the images to load.)");
+			ArrayList<ImageIcon> images = addImagesFromDirectory(directory);
+			if (images.size() >= 2) {
+				System.out.println("-----");
+				JFrame frame = new JFrame("Image Comparator");
+				ImageComparatorPanel panel = new ImageComparatorPanel(frame, images);
+				frame.setContentPane(panel);
+				int frameWidth = 1280;
+				int frameHeight = 720;
+				if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
+					frameHeight += 22;
+				} else if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+					frameWidth += 16;
+					frameHeight += 39;
 				}
+				frame.setSize(frameWidth, frameHeight);
+				frame.setLocationRelativeTo(null);
+				frame.setResizable(false);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
 			} else {
-				System.out.println("Error: " + directoryLocation + " is not a valid directory.");
+				System.out.println("Error: " + directoryLocation + " contains less than 2 images.");
 			}
 		} else {
-			System.out.println("This program's usage is as follows:");
-			System.out.println("java ImageComparator");
-			System.out.println("java ImageComparator <directory location>");
+			System.out.println("Error: " + directoryLocation + " is not a valid directory.");
 		}
 	}
 	
