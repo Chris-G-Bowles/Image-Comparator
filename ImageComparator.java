1d19db93bcd1c6760a951ba1a1dc4474fd9d22b2
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-25 21:58:11 -0700

    Completely reworked the program error-checking flow.
    The program now works once again.

--- ImageComparator.java
+++ ImageComparator.java
@@ -1,6 +1,7 @@
 //Image Comparator
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -24,52 +25,54 @@ public class ImageComparator {
 		} else {
 			directoryLocation = args[0];
 		}
-		input.close();
 		File directory = new File(directoryLocation);
-		if (directory.isDirectory()) {
-			System.out.println("(Please wait a few seconds for the images to load.)");
-			ArrayList<ImageIcon> images = addImagesFromDirectory(directory);
-			if (images.size() >= 2) {
-				System.out.println("-----");
-				JFrame frame = new JFrame("Image Comparator");
-				ImageComparatorPanel panel = new ImageComparatorPanel(frame, images);
-				frame.setContentPane(panel);
-				int frameWidth = 1280;
-				int frameHeight = 720;
-				if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
-					frameHeight += 22;
-				} else if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
-					frameWidth += 16;
-					frameHeight += 39;
-				}
-				frame.setSize(frameWidth, frameHeight);
-				frame.setLocationRelativeTo(null);
-				frame.setResizable(false);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-			} else {
-				System.out.println("Error: " + directoryLocation + " contains less than 2 images.");
-			}
-		} else {
-			System.out.println("Error: " + directoryLocation + " is not a valid directory.");
+		if (!directory.isDirectory()) {
+			error(directoryLocation + " is not a valid directory.");
 		}
+		input.close();
+		System.out.println("(Please wait a few seconds for the images to load.)");
+		ArrayList<ImageIcon> images = addImagesFromDirectory(directory);
+		if (images.size() < 2) {
+			error(directoryLocation + " contains less than 2 images.");
+		}
+		System.out.println("-----");
+		JFrame frame = new JFrame("Image Comparator");
+		ImageComparatorPanel panel = new ImageComparatorPanel(frame, images);
+		frame.setContentPane(panel);
+		int frameWidth = 1280;
+		int frameHeight = 720;
+		if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
+			frameHeight += 22;
+		} else if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+			frameWidth += 16;
+			frameHeight += 39;
+		}
+		frame.setSize(frameWidth, frameHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 	
 	private static ArrayList<ImageIcon> addImagesFromDirectory(File directory) {
-		File[] files = directory.listFiles();
 		ArrayList<ImageIcon> images = new ArrayList<>();
-		for (int i = 0; i < files.length; i++) {
-			if (files[i].isFile()) {
+		for (File file : directory.listFiles()) {
+			if (file.isFile()) {
+				BufferedImage image;
 				try {
-					ImageIcon image = new ImageIcon(ImageIO.read(files[i]).getScaledInstance(600, 600,
-							Image.SCALE_DEFAULT));
-					image.setDescription(files[i].getPath());
-					images.add(image);
+					image = ImageIO.read(file);
 				} catch (Exception e) {
-					System.out.println("Error: " + files[i].getPath() + " does not contain a readable image.");
+					image = null;
 				}
-			} else if (files[i].isDirectory()) {
-				images.addAll(addImagesFromDirectory(files[i]));
+				if (image == null) {
+					System.out.println(file.getPath() + " does not contain a readable image, and is being skipped.");
+					continue;
+				}
+				ImageIcon scaledImage = new ImageIcon(image.getScaledInstance(600, 600, Image.SCALE_DEFAULT));
+				scaledImage.setDescription(file.getPath());
+				images.add(scaledImage);
+			} else if (file.isDirectory()) {
+				images.addAll(addImagesFromDirectory(file));
 			}
 		}
 		return images;
